f3c82ab72a04a78f3b59583ada9b1135d8da1513
yuling1116 <j21028059@gmail.com>
2025-07-22 02:52:55 +0800

    Update main2.cpp

--- hw/src/main2.cpp
+++ hw/src/main2.cpp
@@ -1,7 +1,22 @@
-// main.cpp
 #include <iostream>
 #include "header.h"
 using namespace std;
+
+void generatePowerset(const vector<char>& set, int index,
+                      vector<char>& current, vector<vector<char>>& result) {
+    if (index == set.size()) {
+        result.push_back(current);
+        return;
+    }
+
+    // 不選 set[index]
+    generatePowerset(set, index + 1, current, result);
+
+    // 選 set[index]
+    current.push_back(set[index]);
+    generatePowerset(set, index + 1, current, result);
+    current.pop_back();
+}
 
 int main() {
     vector<char> set = {'a', 'b', 'c'};
